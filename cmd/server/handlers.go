ad32f267e35445ea3c7179cf452a3e27600f5c7f
Yury Lifanov <yulifanov@sharxdc.ru>
2024-11-25 22:20:02 +0300

    rewrite handlers

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -15,31 +15,38 @@ func gaugeHandle(res http.ResponseWriter, req *http.Request) {
 	// }
 
 	path := req.URL.Path
-	match, err := regexp.MatchString(`^/update/gauge/[a-zA-A]+/(\d+\.\d+|\d+)$`, path)
-	if err != nil {
-		http.Error(res, err.Error(), http.StatusInternalServerError)
-	}
-	if !match {
-		http.Error(res, "Invalid URL", http.StatusBadRequest)
-	}
+	if match, err := regexp.MatchString("^/update/gauge/[a-zA-A]+", path); err != nil || !match{
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+		} else if !match {
+			res.WriteHeader(http.StatusNotFound)
+		}
+	} else if match, err := regexp.MatchString(`^/update/gauge/[a-zA-A]+/(\d+\.\d+|\d+)$`, path); err != nil || !match {
+		if err != nil {
+			http.NotFound(res, req)
+		} else if !match {
+			res.WriteHeader(http.StatusBadRequest)
+		}
+	} else {
+		path = strings.Replace(path, "/update/", "", 1)
+		path = strings.Replace(path, "/", " ", -1)
 
-	path = strings.Replace(path, "/update/", "", 1)
-	path = strings.Replace(path, "/", " ", -1)
-
-	var mType, mName string
-	var mValue float64
-	_, err = fmt.Sscanf(path, "%s %s %g", &mType, &mName, &mValue)
-	if err != nil {
-		http.Error(res, err.Error(), http.StatusInternalServerError)
+		var mType, mName string
+		var mValue float64
+		_, err := fmt.Sscanf(path, "%s %s %g", &mType, &mName, &mValue)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+		} else {
+			// storage := GetMemStorage()
+			// _, err = json.Marshal(storage)
+			// if err != nil {
+			// 	http.Error(res, err.Error(), http.StatusInternalServerError)
+			// }
+			res.WriteHeader(http.StatusOK)
+		}
 	}
-	storage := GetMemStorage()
-	storage.SetGauge(mName, mValue)
-	// _, err = json.Marshal(storage)
-	// if err != nil {
-	// 	http.Error(res, err.Error(), http.StatusInternalServerError)
-	// }
-	msg := fmt.Sprintf("Привет, я установил метрику %s в значение %g", mName, mValue)
-	res.Write([]byte(msg))
+	res.Header().Set("Content-type", "text/plain")
+	res.Write([]byte("Привет, я ниего не умею"))
 }
 
 func counterHandle(res http.ResponseWriter, req *http.Request) {
@@ -50,27 +57,32 @@ func counterHandle(res http.ResponseWriter, req *http.Request) {
 	// }
 
 	path := req.URL.Path
-	match, err := regexp.MatchString(`^/update/counter/[a-zA-A]+/(\d+\.\d+|\d+)$`, path)
-	if err != nil {
-		http.Error(res, err.Error(), http.StatusInternalServerError)
+
+	if match, err := regexp.MatchString("^/update/counter/[a-zA-A]+", path); err != nil || !match{
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+		} else if !match {
+			res.WriteHeader(http.StatusNotFound)
+		}
+	} else if match, err := regexp.MatchString(`^/update/counter/[a-zA-A]+/\d+$`, path); err != nil || !match {
+		if err != nil {
+			http.NotFound(res, req)
+		} else if !match {
+			res.WriteHeader(http.StatusBadRequest)
+		}
+	} else {
+		path = strings.Replace(path, "/update/", "", 1)
+		path = strings.Replace(path, "/", " ", -1)
+
+		var mType, mName string
+		var mValue float64
+		_, err = fmt.Sscanf(path, "%s %s %g", &mType, &mName, &mValue)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+		} else {
+			res.WriteHeader(http.StatusOK)
+		}
 	}
-	if !match {
-		http.Error(res, "Invalid URL", http.StatusBadRequest)
-	}
-
-	path = strings.Replace(path, "/update/", "", 1)
-	path = strings.Replace(path, "/", " ", -1)
-
-	var mType, mName string
-	var mValue int64
-	_, err = fmt.Sscanf(path, "%s %s %d", &mType, &mName, &mValue)
-	if err != nil {
-		http.Error(res, err.Error(), http.StatusInternalServerError)
-	}
-
-	storage := GetMemStorage()
-	storage.SetConunter(mName, mValue)
-
-	msg := fmt.Sprintf("Привет, я увеличил метрику %s на %d", mName, mValue)
-	res.Write([]byte(msg))
+	res.Header().Set("Content-type", "text/plain")
+	res.Write([]byte("Привет, я ниего не умею"))
 }
