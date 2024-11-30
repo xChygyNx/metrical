b7a6dcbe75fc7573422e381cb9f68918d5b92fde
Chykh <ChykCha112@yandex.ru>
2024-11-30 20:04:03 +0300

    change method of request on Post

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
 	"net/http"
 )
 
+func routing() error {
+	router := chi.NewRouter()
+	router.Post("/", BadRequestHandle)
+	router.Post("/update/gauge/{metric}/{value}", GaugeHandle)
+	router.Post("/update/counter/{metric}/{value}", CounterHandle)
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", BadRequestHandle)
-	mux.HandleFunc("/update/gauge/", GaugeHandle)
-	mux.HandleFunc("/update/counter/", CounterHandle)
-	err := http.ListenAndServe(":8080", mux)
+	err := routing()
+
 	if err != nil {
 		panic(err)
 	}
