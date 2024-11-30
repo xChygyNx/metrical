b7e0c681f92bb09d0982b72569574c60c80eadb7
Chykh <ChykCha112@yandex.ru>
2024-11-30 17:47:43 +0300

    rewrite on chi routing

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"github.com/go-chi/chi/v5"
 	"net/http"
-	"regexp"
-	"strings"
 )
 
 func BadRequestHandle(res http.ResponseWriter, req *http.Request) {
@@ -15,37 +13,17 @@ func BadRequestHandle(res http.ResponseWriter, req *http.Request) {
 func GaugeHandle(res http.ResponseWriter, req *http.Request) {
 
 	res.Header().Set("Content-type", "text/plain")
-	path := req.URL.Path
-	match, err := regexp.MatchString(`^\/update\/gauge\/[a-zA-A]+`, path)
-	if err != nil {
-		res.WriteHeader(http.StatusInternalServerError)
+	metric := chi.URLParam(req, "metric")
+	if metric == "" {
+		http.Error(res, "Metric param is missed", http.StatusInternalServerError)
 		return
 	}
-	if !match {
-		res.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	match, err = regexp.MatchString(`^\/update\/gauge\/[a-zA-Z]+\/(\d+\.\d+|\d+)$`, path)
-	if err != nil {
-		res.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	if !match {
-		res.WriteHeader(http.StatusBadRequest)
+	value := chi.URLParam(req, "value")
+	if value == "" {
+		http.Error(res, "Value of metric is missed", http.StatusBadRequest)
 		return
 	}
 
-	path = strings.Replace(path, "/update/", "", 1)
-	path = strings.Replace(path, "/", " ", -1)
-
-	var mType, mName string
-	var mValue float64
-	_, err = fmt.Sscanf(path, "%s %s %g", &mType, &mName, &mValue)
-	if err != nil {
-		res.WriteHeader(http.StatusInternalServerError)
-		return
-	}
 	data, err := json.Marshal(map[string]string{"status": http.StatusText(http.StatusOK)})
 	if err != nil {
 		res.WriteHeader(http.StatusInternalServerError)
@@ -58,37 +36,17 @@ func GaugeHandle(res http.ResponseWriter, req *http.Request) {
 func CounterHandle(res http.ResponseWriter, req *http.Request) {
 
 	res.Header().Set("Content-type", "text/plain")
-	path := req.URL.Path
-
-	match, err := regexp.MatchString(`^\/update\/counter\/[a-zA-A]+`, path)
-	if err != nil {
-		res.WriteHeader(http.StatusInternalServerError)
+	metric := chi.URLParam(req, "metric")
+	if metric == "" {
+		http.Error(res, "Metric param is missed", http.StatusInternalServerError)
 		return
 	}
-	if !match {
-		res.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	match, err = regexp.MatchString(`^\/update\/counter\/[a-zA-Z]+\/\d+$`, path)
-	if err != nil {
-		http.NotFound(res, req)
-		return
-	} else if !match {
-		res.WriteHeader(http.StatusBadRequest)
+	value := chi.URLParam(req, "value")
+	if value == "" {
+		http.Error(res, "Value of metric is missed", http.StatusBadRequest)
 		return
 	}
 
-	path = strings.Replace(path, "/update/", "", 1)
-	path = strings.Replace(path, "/", " ", -1)
-
-	var mType, mName string
-	var mValue int64
-	_, err = fmt.Sscanf(path, "%s %s %d", &mType, &mName, &mValue)
-	if err != nil {
-		res.WriteHeader(http.StatusInternalServerError)
-		return
-	}
 	data, err := json.Marshal(map[string]string{"status": http.StatusText(http.StatusOK)})
 	if err != nil {
 		res.WriteHeader(http.StatusInternalServerError)
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/xChygyNx/metrical
 
 go 1.22.9
 
-require github.com/stretchr/testify v1.10.0
+require (
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/stretchr/testify v1.10.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
