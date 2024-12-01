db9faba2a349909e243f9c8fa30eb46f2a21fb34
Chykh <ChykCha112@yandex.ru>
2024-12-01 19:51:12 +0300

    add parse flag for server

---
+++ cmd/server/flag_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseFlags(t *testing.T) {
+	type want struct {
+		port int
+		host string
+	}
+	tests := []struct {
+		name string
+		want want
+	}{
+		{
+			name: "Default values",
+			want: want{
+				port: 8080,
+				host: "localhost",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			hostPort := parseFlag()
+			assert.Equal(t, hostPort.Host, test.want.host)
+			assert.Equal(t, hostPort.Port, test.want.port)
+		})
+	}
+}
---
+++ cmd/server/flags.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type HostPort struct {
+	Host string
+	Port int
+}
+
+func (hp *HostPort) String() string {
+	return fmt.Sprintf("%s:%d", hp.Host, hp.Port)
+}
+
+func (hp *HostPort) Set(value string) error {
+	hostPort := strings.Split(value, ":")
+	if len(hostPort) != 2 {
+		return errors.New(fmt.Sprintf("Must be vlue like <Host>:<Port>, got %s", value))
+	}
+	port, err := strconv.Atoi(hostPort[1])
+	if err != nil {
+		return err
+	}
+	hp.Host = hostPort[0]
+	hp.Port = port
+	return nil
+}
+
+func parseFlag() *HostPort {
+	hostPort := new(HostPort)
+	flag.Var(hostPort, "a", "Net address host:port")
+	flag.Parse()
+	if hostPort.Host == "" && hostPort.Port == 0 {
+		hostPort.Host = "localhost"
+		hostPort.Port = 8080
+	}
+	return hostPort
+}
