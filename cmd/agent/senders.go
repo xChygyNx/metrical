71157cd6fc4afdc4c3245ee2e417477397cc5412
Chykh <ChykCha112@yandex.ru>
2024-12-03 21:15:08 +0300

    fix url path in senders

--- cmd/agent/senders.go
+++ cmd/agent/senders.go
@@ -17,7 +17,7 @@ func SendGauge(client *http.Client, sendInfo []uint8, hostAddr HostPort) error {
 		return err
 	}
 	for attr, value := range mapInfo {
-		urlString := "http://" + fmt.Sprint(hostAddr) + "/update/gauge/" + attr + "/" + strconv.FormatFloat(value, 'f', -1, 64)
+		urlString := "http://" + hostAddr.String() + "/update/gauge/" + attr + "/" + strconv.FormatFloat(value, 'f', -1, 64)
 		req, err := http.NewRequest(http.MethodPost, urlString, bytes.NewBuffer([]byte(sendInfo)))
 		if err != nil {
 			return err
@@ -42,7 +42,7 @@ func SendGauge(client *http.Client, sendInfo []uint8, hostAddr HostPort) error {
 }
 
 func SendCounter(client *http.Client, pollCount int, hostAddr HostPort) error {
-	counterPath := "http://" + fmt.Sprint(hostAddr) + "/update/counter/PollCount/" + strconv.Itoa(pollCount) + ":8080"
+	counterPath := "http://" + hostAddr.String() + "/update/counter/PollCount/" + strconv.Itoa(pollCount)
 	req, err := http.NewRequest(http.MethodPost, counterPath, bytes.NewBuffer([]byte(strconv.Itoa(pollCount))))
 	if err != nil {
 		panic(err)
