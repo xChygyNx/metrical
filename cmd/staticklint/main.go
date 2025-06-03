61ca976e824a328af69f216a936c648cbaea5eac
Chykh <ChykCha112@yandex.ru>
2025-06-03 21:44:05 +0300

    second iter

--- cmd/staticklint/main.go
+++ cmd/staticklint/main.go
@@ -13,7 +13,15 @@ import (
 	"strings"
 )
 
-var STATICCHECK_SA = []string{"sa1000", "sa1001", "sa1002", "sa1003", "sa1004", "sa1005", "sa1006", "sa1007", "sa1008", "sa1010", "sa1011", "sa1012", "sa1013", "sa1014", "sa1015", "sa1016", "sa1017", "sa1018", "sa1019", "sa1020", "sa1021", "sa1023", "sa1024", "sa1025", "sa1026", "sa1027", "sa1028", "sa1029", "sa1030", "sa1031", "sa1032", "sa2000", "sa2001", "sa2002", "sa2003", "sa3000", "sa3001", "sa4000", "sa4001", "sa4003", "sa4004", "sa4005", "sa4006", "sa4008", "sa4009", "sa4010", "sa4011", "sa4012", "sa4013", "sa4014", "sa4015", "sa4016", "sa4017", "sa4018", "sa4019", "sa4020", "sa4021", "sa4022", "sa4023", "sa4024", "sa4025", "sa4026", "sa4027", "sa4028", "sa4029", "sa4030", "sa4031", "sa4032", "sa5000", "sa5001", "sa5002", "sa5003", "sa5004", "sa5005", "sa5007", "sa5008", "sa5009", "sa5010", "sa5011", "sa5012", "sa6000", "sa6001", "sa6002", "sa6003", "sa6005", "sa6006", "sa9001", "sa9002", "sa9003", "sa9004", "sa9005", "sa9006", "sa9007", "sa9008", "sa9009"}
+func isTestFile(pkgPath string) bool {
+	return strings.Contains(pkgPath, "_test.go")
+}
+
+func isLocalPackage(pkgPath string) bool {
+	const projectPath = "github.com/xChygyNx/metrical"
+	projectPathPrefix := projectPath + "/"
+	return pkgPath == projectPath || strings.HasPrefix(pkgPath, projectPathPrefix)
+}
 
 // ErrOsExitAnalizer анализатор фиксирующий использование функции os.Exit в main функциях
 var ErrOsExitAnalizer = &analysis.Analyzer{
@@ -23,6 +31,17 @@ var ErrOsExitAnalizer = &analysis.Analyzer{
 }
 
 func checkOsExit(pass *analysis.Pass) (interface{}, error) {
+	pkgPath := pass.Pkg.Path()
+
+	// Проверяем, что пакет находится внутри проекта
+	if !isLocalPackage(pkgPath) {
+		return nil, nil
+	}
+
+	// Проверяем, что это не тест файлы
+	if isTestFile(pkgPath) {
+		return nil, nil
+	}
 	for _, file := range pass.Files {
 		ast.Inspect(file, func(node ast.Node) bool {
 			switch x := node.(type) {
@@ -41,12 +60,19 @@ func checkOsExit(pass *analysis.Pass) (interface{}, error) {
 }
 
 func main() {
+	needStaticChecks := map[string]bool{
+		"S1003":  true, // Replace call to strings.Index with strings.Contains
+		"ST1008": true, // A function’s error value should be its last return value
+		"QF1004": true, // Use strings.ReplaceAll instead of strings.Replace with n == -1
+	}
 	mychecks := make([]*analysis.Analyzer, 0, len(staticcheck.Analyzers))
 	for _, analys := range staticcheck.Analyzers {
-		if strings.HasPrefix(strings.ToUpper(analys.Analyzer.Name), "SA") {
+		_, ok := needStaticChecks[strings.ToUpper(analys.Analyzer.Name)]
+		if strings.HasPrefix(strings.ToUpper(analys.Analyzer.Name), "SA") || ok {
 			mychecks = append(mychecks, analys.Analyzer)
 		}
 	}
+
 	publicAnalyzers := []*analysis.Analyzer{
 		printf.Analyzer,    // анализатор, фиксирующий несоответствие масок вывода типам выводимых агументов
 		shadow.Analyzer,    // анализатор, фиксирующий "затенение" внешних переменных локальными переменными
