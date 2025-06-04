ccfcad1c21e6f38035edd581b091c037d9e07358
Chykh <ChykCha112@yandex.ru>
2025-06-04 22:48:56 +0300

    output tech info

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	_ "net/http/pprof" // требуется для запуска профилировщика
+	"os/exec"
+	"time"
 
 	"github.com/xChygyNx/metrical/internal/server"
 )
 
+func getLastCommit() string {
+	out, err := exec.Command("git", "rev-parse", "HEAD").Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+	commitHash := string(out)
+	return commitHash
+}
+
+var buildVersion = "1.0.0"
+var buildDate = time.Now().Format("02-01-2006")
+var buildCommit = getLastCommit()
+
 func main() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
 	err := server.Routing()
 
 	if err != nil {
