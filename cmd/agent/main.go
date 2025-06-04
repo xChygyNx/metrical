af75ba71e4fa465ed87a6eb5e5a4693b14c0a28a
Chykh <ChykCha112@yandex.ru>
2025-06-04 22:48:56 +0300

    output tech info

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os/exec"
+	"time"
 
 	"github.com/xChygyNx/metrical/internal/agent"
 )
 
+func getLastCommit() string {
+	out, err := exec.Command("git", "rev-parse", "HEAD").Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+	commitHash := string(out)
+	return commitHash
+}
+
+var buildVersion = "1.0.0"
+var buildDate = time.Now().Format("02-01-2006")
+var buildCommit = getLastCommit()
+
 func main() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
 	err := agent.Run()
 	if err != nil {
 		log.Fatal(err)
