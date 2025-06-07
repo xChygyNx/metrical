cca09180a1725262e60a479f43b936d0eb1a9b9d
Chykh <ChykCha112@yandex.ru>
2025-06-07 13:44:49 +0300

    add handle ldflags foe server + add doc strings

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -6,9 +6,14 @@ import (
 	"log"
 )
 
-var buildVersion string = "N/A"
-var buildDate string = "N/A"
-var buildCommit string = "N/A"
+// For output tech info need launch app with ldflags
+//
+//	-X main.buildVersion=<buildVersion> for output "Build version: <buildVersion>" (default N/A)
+//	-X main.buildDate=$(date +'<date_format>') for output "Build date: <date_in_format>" (default N/A)
+//	-X main.buildCommit=$(git rev-parse HEAD) for output "Build commit: <last_commit_hash>" (default N/A)
+var buildVersion string = "N/A" // Server version
+var buildDate string = "N/A"    // Date of Build
+var buildCommit string = "N/A"  // Hash of the last commit
 
 func main() {
 	fmt.Printf("Build version: %s\n", buildVersion)
