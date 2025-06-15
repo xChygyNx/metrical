6b2f3992196bc282487628649eb91883fc7b1f68
Chykh <ChykCha112@yandex.ru>
2025-06-15 22:31:49 +0300

    implement read parameters from JSON

--- internal/server/config.go
+++ internal/server/config.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
@@ -34,13 +35,23 @@ type HostPort struct {
 
 // Config структура для хранение конфигураций сервера.
 type Config struct {
-	FileStoragePath string   // Путь до файла для сохранения метрик.
-	DBAddress       string   // Строка подключения к базе данных PostgreSQL.
-	RSAPrivateKey   string   // Путь до файла с приватным ключом.
+	FileStoragePath string   `json:"store_file"`   // Путь до файла для сохранения метрик.
+	DBAddress       string   `json:"database_dsn"` // Строка подключения к базе данных PostgreSQL.
+	RSAPrivateKey   string   `json:"crypto_key"`   // Путь до файла с приватным ключом.
 	ConfigFile      string   // JSON файл с конфигурацией сервера
-	HostPort        HostPort // Адрес сервера.
-	StoreInterval   int      // Интервал сохранения метрик
-	Restore         bool     // Флаг загрузки метрик из хранилища при старте сервера
+	HostPort        HostPort `json:"address"`       // Адрес сервера.
+	StoreInterval   int      `json:"storeInterval"` // Интервал сохранения метрик
+	Restore         bool     `json:"restore"`       // Флаг загрузки метрик из хранилища при старте сервера
+}
+
+// TmpConfig структура для хранения параметров сервера, считанных из JSON файла.
+type TmpConfig struct {
+	FileStoragePath string `json:"store_file"`    // Путь до файла для сохранения метрик.
+	DBAddress       string `json:"database_dsn"`  // Строка подключения к базе данных PostgreSQL.
+	RSAPrivateKey   string `json:"crypto_key"`    // Путь до файла с приватным ключом.
+	HostPort        string `json:"address"`       // Адрес сервера.
+	StoreInterval   int    `json:"storeInterval"` // Интервал сохранения метрик
+	Restore         bool   `json:"restore"`       // Флаг загрузки метрик из хранилища при старте сервера
 }
 
 // String представляет данные из структуры HostPort в текстовом формате "host:port".
@@ -87,13 +98,62 @@ func parseFlag() *Config {
 	flag.BoolVar(&config.Restore, "r", true, "Define should or not load store data from file before start")
 	flag.StringVar(&config.DBAddress, "d", "", "Address of connecting to Data Base")
 	flag.StringVar(&config.RSAPrivateKey, "crypto-key", "", "Path to RSA private key")
-	flag.StringVar(&config.RSAPrivateKey, "crypto-key", "", "Path to RSA private key")
+	flag.StringVar(&config.ConfigFile, "config", "", "Path to JSON config file")
+	flag.StringVar(&config.ConfigFile, "c", "", "Path to JSON config file")
 	flag.Parse()
 	if config.HostPort.Host == "" && config.HostPort.Port == 0 {
 		config.HostPort.Host = "localhost"
 		config.HostPort.Port = 8080
 	}
 	return config
+}
+
+func getArgsWithoutDashes() map[string]bool {
+	args := make(map[string]bool, 0)
+	for _, elem := range os.Args {
+		if strings.HasPrefix(elem, "-") {
+			elem = strings.TrimLeft(elem, "-")
+			args[elem] = true
+		}
+	}
+	return args
+}
+
+func parseConfigFromJSON(configFile string, config *Config) (*Config, error) {
+	fileData, err := os.ReadFile(configFile)
+	if err != nil {
+		return nil, fmt.Errorf("error in read file %s: %w", configFile, err)
+	}
+	tmpConfig := &TmpConfig{}
+	err = json.Unmarshal(fileData, tmpConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error in unmarshal json data: %w", err)
+	}
+	args := getArgsWithoutDashes()
+
+	if _, ok := args["a"]; !ok {
+		err = config.HostPort.Set(tmpConfig.HostPort)
+		if err != nil {
+			return nil, fmt.Errorf("error in parse HostPort from JSON: %w", err)
+		}
+	}
+	if _, ok := args["i"]; !ok {
+		config.StoreInterval = tmpConfig.StoreInterval
+	}
+	if _, ok := args["f"]; !ok {
+		config.FileStoragePath = tmpConfig.FileStoragePath
+	}
+	if _, ok := args["r"]; !ok {
+		config.Restore = tmpConfig.Restore
+	}
+	if _, ok := args["d"]; !ok {
+		config.DBAddress = tmpConfig.DBAddress
+	}
+	if _, ok := args["crypto-key"]; !ok {
+		config.RSAPrivateKey = tmpConfig.RSAPrivateKey
+	}
+
+	return config, nil
 }
 
 // GetConfig возвращает структуру config в которой заданы такие параметры
@@ -106,14 +166,22 @@ func parseFlag() *Config {
 // 1) Переменные окружения.
 // 2) Аргументы командной строки.
 // 3) Файл конфигурации из параметра -c/-config или переменной окружения CONFIG
-func GetConfig() (*Config, error) {
-	config := parseFlag()
+func GetConfig() (config *Config, err error) {
+	config = parseFlag()
 
-	//configFileEnv, ok := os.LookupEnv("CONFIG")
-	//configFileArg := config.
-	//if ok {
-	//
-	//}
+	configFileEnv, ok := os.LookupEnv("CONFIG")
+	configFileArg := config.ConfigFile
+	if ok {
+		config, err = parseConfigFromJSON(configFileEnv, config)
+		if err != nil {
+			return nil, fmt.Errorf("error parse config from JSON from env: %w", err)
+		}
+	} else if configFileArg != "" {
+		config, err = parseConfigFromJSON(configFileArg, config)
+		if err != nil {
+			return nil, fmt.Errorf("error parse config from JSON from args: %w", err)
+		}
+	}
 
 	hostAddr, ok := os.LookupEnv("ADDRESS")
 	if ok {
